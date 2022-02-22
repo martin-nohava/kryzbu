3266f21053468c55b5eba3bc1153d4200ea24e94
Bloc3k <calalamadolo@gmail.com>
2022-02-22 18:18:05 +0100

    Added TCP communication showcase

--- src/server.py
+++ src/server.py
@@ -1,1 +1,22 @@
 # Server side
+
+import socket
+
+
+def example_comm_s():
+    "Receive 1024b block from TCP stream at port 50000."
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('localhost', 50000))
+
+    server.listen(1)
+    conn, addr = server.accept()
+
+    data = conn.recv(1024)
+
+    server.close()
+
+    print(data)
+
+if __name__ == '__main__':
+    example_comm_s()
