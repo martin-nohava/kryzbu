903acf43f752de0fe781eb22dfe257dad101aaa0
Bloc3k <calalamadolo@gmail.com>
2022-02-24 17:31:18 +0100

    Plaintext file transfare

--- src/client/client.py
+++ src/client/client.py
@@ -1,18 +1,52 @@
 # Client side
 
 import socket
+import asyncio
+import os
 
 class Client:
     """Implementation of a Kryzbu client."""
-    
+
     @staticmethod
-    def example_comm():
+    def send_file():
+        
+        SEPARATOR = "\\"
+        BUFFER_SIZE = 4096
+
+        s = socket.socket()
+        host = "127.0.0.1"
+        port = 60606
+        print(f"[+] Connecting to {host}:{port}")
+        s.connect((host, port))
+        print("[+] Connected to ", host)
+        filename = input('Enter file name: ')
+        filesize = os.path.getsize(filename)
+        s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+
+        #progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(filename, "rb") as f:
+            while True:
+                bytes_read = f.read(BUFFER_SIZE)
+                if not bytes_read:
+                    break
+                s.sendall(bytes_read)
+                #progress.update(len(bytes_read))
+        s.close()
+
+    @staticmethod
+    def open_n_hold():
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as handle:
+            handle.connect(('localhost', 60606))
+
+
+    @staticmethod
+    async def example_comm():
         '''Send b"Hello, word" to localhost:50000 over TCP.'''
 
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(('localhost', 50000))
+        client.connect(('localhost', 60606))
         client.sendall(b'Hello, world')
         client.close()
 
 if __name__ == '__main__':
-    Client.example_comm()
+    Client.send_file()
--- src/server/server.py
+++ src/server/server.py
@@ -1,10 +1,84 @@
 # Server side
 
 import socket
-from typing import Tuple
+import os
+
 
 class Server:
     """Implementation of a Kryzbu server (Cryptographically Secure Storage)."""
+
+    # Static variables
+    IP_ADDR: str = None
+    PORT: int = None
+
+    # List of opened connections
+    open_conns = [] 
+
+    def __init__(self, ip_addr: str = 'localhost'):
+        """Constructor initialize IP_ADDR (127.0.0.1 by default) and PORT (60606 by default) and
+        bind TCP connection, but doesn't start listenning."""
+
+        Server.IP_ADDR = ip_addr
+
+        self.server: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+    def listen_for_connection(self, queue_size: int = 1, port: int = 60606):
+        """Listen for TCP connections on specified port. Incoming connections
+        are accepted as long as there is space in queue. Accepted connections
+        are added to open_conns list.
+
+        Parameters:
+            queue_size (int): Size of queue for incoming connections (default 1).
+            port (int): Port to listen on (default 60606).
+
+        Returns:
+            (socket): socket with accepted connection
+            (str): IPv4 address (conn, src_addr)."""
+        Server.PORT = port
+        self.server.bind((Server.IP_ADDR, Server.PORT))  # TODO: Can throw exceptions
+        self.server.listen(queue_size)
+        conn, src_addr = self.server.accept()
+        return conn, src_addr
+
+
+    def close_connection(self, connection: socket.socket):
+        """Close specified TCP connection.
+        
+        Parameters:
+            connection (socket): Connection to close."""
+        connection.close()
+
+    
+    @staticmethod
+    def recieve_file():
+        SERVER_HOST = "127.0.0.1"
+        SERVER_PORT = 60606
+        BUFFER_SIZE = 4096
+        SEPARATOR = "\\"
+
+        s = socket.socket()
+        s.bind((SERVER_HOST, SERVER_PORT))
+        s.listen(10)
+        print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+        print("Waiting for the client to connect... ")
+        client_socket, address = s.accept()
+        print(f"[+] {address} is connected.")
+        received = client_socket.recv(BUFFER_SIZE).decode()
+        filename, filesize = received.split(SEPARATOR)
+        filename = os.path.basename(filename)
+        filesize = int(filesize)
+        #progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(filename, "wb") as f:
+            while True:
+                bytes_read = client_socket.recv(BUFFER_SIZE)
+                if not bytes_read:
+                    break
+                f.write(bytes_read)
+                #progress.update(len(bytes_read))
+        client_socket.close()
+        s.close()
+
 
     @staticmethod
     def example_comm():
@@ -24,4 +98,4 @@ class Server:
     
 
 if __name__ == '__main__':
-    Server.example_comm()
+    Server.recieve_file()
