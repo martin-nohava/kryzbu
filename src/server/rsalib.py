c9af47f58aaae014f7b13a1de07a33c24399b9e6
Martin <martin.nohava.mn@gmail.com>
2022-03-10 14:52:50 +0100

    Switched craptography lib for pycryptodome
    Updated requirements and RSA key-pair generation

--- src/server/rsalib.py
+++ src/server/rsalib.py
@@ -1,11 +1,9 @@
 import os
 from pathlib import Path
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
+from Crypto.PublicKey import RSA
 
 class Rsa:
 
-  PUBLIC_EXPONENT = 65537
   KEY_SIZE = 2048
   KEY_PATH = Path('server/_data/keys/')
   PRIVATE_KEY_PASS = b"pivate-key-will-be-encrypted-with-this-pass"
@@ -18,29 +16,16 @@ class Rsa:
   def init():
     # Check if keypair exists in filesystem
     if not os.path.exists(Rsa.KEY_PATH / Rsa.KEY_FILE_NAMES[0]) or not os.path.exists(Rsa.KEY_PATH / Rsa.KEY_FILE_NAMES[1]):
-      private_key = rsa.generate_private_key(
-          Rsa.PUBLIC_EXPONENT,
-          Rsa.KEY_SIZE
-      )
+      key = RSA.generate(Rsa.KEY_SIZE)
+      private_key = key.export_key(passphrase=Rsa.PRIVATE_KEY_PASS, pkcs=8, protection="scryptAndAES128-CBC")
+      file_out = open(Rsa.KEY_PATH / Rsa.KEY_FILE_NAMES[0], "wb")
+      file_out.write(private_key)
+      file_out.close()
 
-      encrypted_pem_private_key = private_key.private_bytes(
-          encoding=serialization.Encoding.PEM,
-          format=serialization.PrivateFormat.PKCS8,
-          encryption_algorithm=serialization.BestAvailableEncryption(Rsa.PRIVATE_KEY_PASS)
-      )
-
-      pem_public_key = private_key.public_key().public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-      )
-
-      private_key_file = open(Rsa.KEY_PATH / Rsa.KEY_FILE_NAMES[0], "w")
-      private_key_file.write(encrypted_pem_private_key.decode())
-      private_key_file.close()
-
-      public_key_file = open(Rsa.KEY_PATH / Rsa.KEY_FILE_NAMES[1], "w")
-      public_key_file.write(pem_public_key.decode())
-      public_key_file.close()
+      public_key = key.publickey().export_key()
+      file_out = open(Rsa.KEY_PATH / Rsa.KEY_FILE_NAMES[1], "wb")
+      file_out.write(public_key)
+      file_out.close()
 
       print('INFO: RSA key-pair generated.')
     else:
